e33e6383d8bdcbc304934f9f049500f0b5c5f9ed
Trisha Mallya <trisha_mallya@yahoo.com>
2021-05-22 22:52:03 -0500

    Completed app.js

--- js/app.js
+++ js/app.js
@@ -21,4 +21,26 @@ function buildTable(data) {
             }
         );
     });
-}
+};
+
+function handleClick() {
+    // Grab datetime value from filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check if date was entered and filter data using date
+    if (date) {
+        // Apply `filter` to table data to only keep rows where `datetime`===filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild table using filtered data
+    // @NOTE: If no date entered, filteredData will just be original tableData
+    buildTable(filteredData);
+};
+
+// Attach an event to listen for form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build table when page loads
+buildTable(tableData);
